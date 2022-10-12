9a2b4621c73088f6b2858f005bb863896862ad28
Imran Azam <imran.azam@and.digital>
2022-10-12 12:15:06 +0100

    removed unnecessary semicols

--- src/main/kotlin/GameGrid.kt
+++ src/main/kotlin/GameGrid.kt
@@ -22,7 +22,7 @@ class GameGrid(var rows: Int, var cols: Int) {
     }
 
     fun computeMinesCount(grid: GridType): GridType {
-        var x = grid;
+        var x = grid
         for (r in 0..rows - 1)
             for (c in 0..cols - 1)
                 x = countCellMines(r, c)
@@ -31,22 +31,22 @@ class GameGrid(var rows: Int, var cols: Int) {
     }
 
     fun countCellMines(x: Int, y: Int): GridType {
-        if (grid[x][y] == "X") return grid;
+        if (grid[x][y] == "X") return grid
 
-        val rowStart = Math.max(x - 1, 0);
-        val rowFinish = Math.min(x + 1, grid.size - 1);
-        val colStart = Math.max(y - 1, 0);
-        val colFinish = Math.min(y + 1, grid.size - 1);
+        val rowStart = Math.max(x - 1, 0)
+        val rowFinish = Math.min(x + 1, grid.size - 1)
+        val colStart = Math.max(y - 1, 0)
+        val colFinish = Math.min(y + 1, grid.size - 1)
 
-        var ctr = 0;
+        var ctr = 0
         for (curRow in rowStart..rowFinish) {
             for (curCol in colStart..colFinish) {
                 if (grid[curRow][curCol] == "X") {
-                    ctr++;
+                    ctr++
                 }
             }
         }
         grid[x][y] = "$ctr"
-        return grid;
+        return grid
     }
 }
