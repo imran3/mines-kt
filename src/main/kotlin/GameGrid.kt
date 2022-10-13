d6f7f43bbc122596a22be394d188cc1a10e27e9e
Imran Azam <imran.azam@and.digital>
2022-10-13 12:32:49 +0100

    refactoring and clean up

--- src/main/kotlin/GameGrid.kt
+++ src/main/kotlin/GameGrid.kt
@@ -7,45 +7,45 @@ class GameGrid(private var rows: Int, private var cols: Int) {
 
     var grid: GridType = Array(rows) { Array(cols) { "-" } }
 
-    fun placeMine(x: Int, y: Int, grid: GridType): GridType {
+    fun placeMine(x: Int, y: Int) {
         grid[x][y] = "X"
-        return grid
     }
 
-    fun printGrid(grid: GridType) {
-        println("\nGame Grid")
+    fun printGrid() {
+        println(gridToString())
+    }
+
+    fun gridToString(): String{
+        var res = ""
         grid.forEach { row ->
-            row.forEach { cell -> print(" $cell ") }
-            println()
+            row.forEach { cell -> res += " $cell " }
+            res += "\n"
         }
+        return res
     }
 
-    fun computeMinesCount(grid: GridType): GridType {
-        var x = grid
+    fun computeMinesCount() {
         for (r in 0 until rows)
             for (c in 0 until cols)
-                x = countCellMines(r, c)
-
-        return x
+                countCellMines(r, c)
     }
 
-    private fun countCellMines(x: Int, y: Int): GridType {
-        if (grid[x][y] == "X") return grid
+    private fun countCellMines(x: Int, y: Int) {
+        if (grid[x][y] != "X") {
+            val rowStart = max(x - 1, 0)
+            val rowFinish = min(x + 1, rows - 1)
+            val colStart = max(y - 1, 0)
+            val colFinish = min(y + 1, cols - 1)
 
-        val rowStart = max(x - 1, 0)
-        val rowFinish = min(x + 1, rows- 1)
-        val colStart = max(y - 1, 0)
-        val colFinish = min(y + 1, cols - 1)
-
-        var ctr = 0
-        for (curRow in rowStart..rowFinish) {
-            for (curCol in colStart..colFinish) {
-                if (grid[curRow][curCol] == "X") {
-                    ctr++
+            var minesCtr = 0
+            for (curRow in rowStart..rowFinish) {
+                for (curCol in colStart..colFinish) {
+                    if (grid[curRow][curCol] == "X") {
+                        minesCtr++
+                    }
                 }
             }
+            grid[x][y] = "$minesCtr"
         }
-        grid[x][y] = "$ctr"
-        return grid
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
     val game = GameGrid(5, 5);
+    game.printGrid()
 
-    game.printGrid(game.grid)
-    game.placeMine(2, 0, game.grid)
-    game.placeMine(2, 1, game.grid)
-    game.placeMine(4, 4, game.grid)
-    game.printGrid(game.grid)
+    game.placeMine(2, 0 )
+    game.placeMine(2, 1)
+    game.placeMine(4, 4)
+    game.printGrid()
 
-    game.computeMinesCount(game.grid)
-    game.printGrid(game.grid)
+    game.computeMinesCount()
+    game.printGrid()
 }
